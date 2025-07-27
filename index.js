234311727aef98427b09a9e9e5166ee483eefba6
wajahat-8 <lowlevelcoding8@gmail.com>
2025-07-27 17:18:32 +0500

    ongodb  is completed

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.get('/', (request, response) => {
 app.get('/api/notes', (request, response) => {
   Note.find({})
     .then(notes => {
-      console.log('Fetched from DB:', notes)
+
       response.json(notes)
     })
     .catch(error => {
@@ -55,15 +55,39 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id).then(note => {
-    response.json(note)
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
   })
+    .catch(error => next(error))
 })
 app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
-  response.status(204).end()
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
+  Note.findById(request.params.id)
+    .then(note => {
+      if (!note) {
+        return response.status(404).end()
+
+      }
+      note.content = content
+      note.important = important
+      return note.save().then((updatedNote) => {
+        response.json(updatedNote)
+
+      })
+
+    }).catch(error => next(error))
 })
 app.post('/api/notes', (request, response) => {
   const body = request.body
@@ -86,7 +110,20 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+
+
 app.use(unknownEndpoint)
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 const PORT = process.env.PORT
 
 app.listen(PORT, () => {
